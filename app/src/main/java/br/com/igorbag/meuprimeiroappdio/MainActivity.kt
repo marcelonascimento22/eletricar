c68edb2baeccda51f344bd61fbe4f9ce6572c26a
Marcelo Nascimento <marcelo.augustonasci@gmail.com>
2026-01-20 09:45:30 -0300

    Primeiro

--- app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
@@ -1,14 +1,54 @@
+
 package br.com.igorbag.meuprimeiroappdio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var preco: EditText
+    lateinit var kmPercorrido: EditText
+    lateinit var btnCalcular: Button
+    lateinit var resultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //O desafio sera criar uma valor dentro do string.xml
-        // E trocar o texto do xml e tornar internacional (Ingles, Espanhol, etc...)
+        setupView()
+        setupListeners()
+
+    }
+    fun setupView(){
+        preco = findViewById(R.id.et_preco_kwh)
+        kmPercorrido = findViewById(R.id.et_km_percorrido)
+        resultado = findViewById(R.id.tv_resultado)
+        btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+    }
+
+    fun setupListeners(){
+
+        btnCalcular.setOnClickListener {
+            calcular()
+        }
+    }
+
+    fun calcular(){
+        val preco = preco.text.toString().toFloat()
+        val km = kmPercorrido.text.toString().toFloat()
+        val result = preco / km
+
+        resultado.text = result.toString()
+
+        Log.d("Testo Digitado -> ", preco.toString())
+        Log.d("Km Percorrido => ", km.toString())
+        Log.d("Resultado -> ", result.toString())
     }
 }
